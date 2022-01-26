9c990d02d3e67026675bfc4c95e960d8221768b8
Hakan D <68669489+hakandrsn@users.noreply.github.com>
2022-01-26 03:23:40 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,4 @@
-call .\venv\scripts\activate
-call pytest -v -s -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
-call pytest -v -s -m "sanity" --html=./Reports/report.html testCases/ --browser firefox
+pytest -v -s -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
 REM pytest -v -s -m "sanity or regression" --html=./Reports/report.html testCases/ --browser chrome
 REM pytest -v -s -m "sanity and regression" --html=./Reports/report.html testCases/ --browser chrome
 REM pytest -v -s -m "regression" --html=./Reports/report.html testCases/ --browser chrome
